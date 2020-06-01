979caccad98b009c7c21e4eab863fb85a1a8fd3b
scorpions <uni.ramos.telecomun@gmail.com>
2020-06-01 02:14:02 -0500

    MaestroPeru con formulario corriendo

--- src/controllers/Principalcontroller.js
+++ src/controllers/Principalcontroller.js
@@ -1,3 +1,4 @@
+var nodemailer = require('nodemailer');
 const principalcontroller = {};
 
 principalcontroller.principal_controller = (req, res) => {
@@ -8,7 +9,33 @@ principalcontroller.principal_controller = (req, res) => {
 }
 
 principalcontroller.mesagetocontact_controller = (req, res)=> {
-        console.log(req.body);
         
+                        
+                var transporter = nodemailer.createTransport({
+                service: 'Gmail',
+                auth: {
+                        user: 'leone.vitrub.uni@gmail.com',
+                        pass: 'vitrubman@leones123456789'
+                }
+                });
+                
+                // configura los datos del correo
+                var mailOptions = {
+                from: "MaestroPeru <leone.vitrub.uni@gmail.com>",
+                to: 'uni.ramos.telecomun@gmail.com',
+                subject: 'Urgente!!! Cliente',
+                text: 'Hola Mundo',
+                html: '<b>Hola Mundo</b>'
+                };
+                
+                // Envía el correo con el objeto de transporte definido anteriormente
+                transporter.sendMail(mailOptions, function(error, info){
+                if(error){
+                        return console.log(error);
+                }
+                
+                console.log('Mensaje enviado: ' + info.response);
+                });
+                        
 }
 module.exports = principalcontroller;
